8bb39b31dc363af31e5d228cd900c58b78af8e64
jun4869 <jun2194k@gmail.com>
2021-12-22 21:39:14 +0900

    devise

--- app/controllers/message_controller.rb
+++
@@ -1,5 +0,0 @@
-class MessageController < ApplicationController
-  def index
-    @messages = Message.order('created_at desc').last(10)
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'message/index'
-  get 'home/index'
-  devise_for :users
-
-  root 'message#index'
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    passwords: 'users/passwords',
+    registrations: 'users/registrations'
+  }
+  root to: 'home#index'
 
 end
