abdc0dbd61cc553a22076607d08e8f008580337f
jun4869 <jun2194k@gmail.com>
2021-12-22 22:05:40 +0900

    ダミーデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-names = ['Mr. Teacher', 'Antonio Banderas', 'Cynthia Erivo', 'Scarlett Johansson', 'Jonathan Pryce', 'Florence Pugh']
+names = ['Mr. Teacher', 'Antonio Banderas', 'Cynthia Erivo', 'Scarlett Johansson', 'Jonathan Pryce', 'Florence Pugh','Yalitza Johansson', 'Olivia Colman', 'Marina de Travira', 'Adam Driver', 'Sam Elliott', 'Lady Gaga', 'Richard E. Grant', 'Regina King']
 
 names.each_with_index{|name, i|
   uid = sprintf "user%03d", i
