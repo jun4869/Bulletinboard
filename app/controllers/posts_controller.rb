4ef8ce2170c0a2b8872c325808bbc915597b1523
jun4869 <jun2194k@gmail.com>
2021-12-28 14:44:41 +0900

    post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,19 @@
 class PostsController < ApplicationController
   def create
+    p = post_params
+    user = User.find(p[:id])
+    if user.posts.create(body: p[:body], submitter: current_user.id)
+      flash[:notice] = 'Post was successfully created.'
+    else
+      flash[:alert] = 'Something went wrong'
+    end
+
+    redirect_to user_path(user.id)
   end
+
+  protected
+  def post_params
+    params.require(:post).permit(:body, :id)
+  end
+  
 end
