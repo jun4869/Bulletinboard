a39e7194b61bed5d6bff75816ac044fd07afcf99
jun4869 <jun2194k@gmail.com>
2021-12-31 14:08:09 +0900

    visit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_28_053647) do
+ActiveRecord::Schema.define(version: 2021_12_29_081233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,13 @@ ActiveRecord::Schema.define(version: 2021_12_28_053647) do
     t.string "role", default: "student"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  create_table "visits", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "visitee"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,5 @@ names = ['Mr. Teacher', 'Antonio Banderas', 'Cynthia Erivo', 'Scarlett Johansson
 names.each_with_index{|name, i|
   uid = sprintf "user%03d", i
   User.create(username:uid, fullname: name, password: '123456', role: ((i==0)? 'teacher' : 'student'))
+  Visit.create(user_id: 1, visitee: i)
 }
