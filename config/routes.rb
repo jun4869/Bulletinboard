dd8cb4df8353c077efc1da62f08052c9717af2f1
jun4869 <jun2194k@gmail.com>
2022-01-01 21:59:03 +0900

    administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+      resources :messages
+      resources :visits
+      resources :posts
+
+      root to: "users#index"
+    end
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     passwords: 'users/passwords',
